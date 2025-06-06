97500e659900185f4bf755e64482a02090842c32
Bikram Parajuli <bikramparajuli0000@gmail.com>
2025-06-06 14:34:34 +0545

    bump

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -2,14 +2,23 @@ import "./i18next"
 import { Toaster } from "react-hot-toast"
 import { DarkModeProvider } from "./components/DarkModeProvider"
 import { QueryProvider } from "@miti/query/provider"
-
 import { BrowserRouter } from "react-router-dom"
 import Body from "./Body"
+import { QueryClient } from "@tanstack/react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+})
 
 const App = () => {
   return (
     <BrowserRouter>
-      <QueryProvider>
+      <QueryProvider client={queryClient}>
         <DarkModeProvider>
           <Body />
           <Toaster position="bottom-center" />
